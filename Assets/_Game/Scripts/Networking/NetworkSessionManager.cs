c4a192e94d2cc2b0d82468b5287194c435e99919
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2023-12-17 16:22:43 +0100

    DataValidation

--- Assets/_Game/Scripts/Networking/NetworkSessionManager.cs
+++ Assets/_Game/Scripts/Networking/NetworkSessionManager.cs
@@ -2,12 +2,22 @@ using ER;
 using UnityEngine;
 using System.Collections.Generic;
 using static GameNetworkData;
+using Unity.VisualScripting;
+using System;
 
 public class NetworkSessionManager : MonoBehaviour
 {
     public static NetworkSessionManager Instance { get; private set; }
 
     [SerializeField] private List<GameNetworkData> _gameNetworkDataList = new List<GameNetworkData>();
+
+    public enum DataValidationErrors
+    {
+        None = 0,
+        EmptyField = 1,
+        PortNumError = 2,
+        IPAddressError = 3,
+    }
 
     private void Awake()
     {
@@ -21,17 +31,93 @@ public class NetworkSessionManager : MonoBehaviour
         }
     }
 
-    public void AddGameNetworkData (ENetMode netMode, string portNum, string iPAddress, string playerName)
+    public DataValidationErrors AddGameNetworkData (ENetMode netMode, string portNum, string iPAddress, string playerName)
     {
+        if(portNum.Length == 0 || iPAddress.Length == 0 || playerName.Length == 0)
+        {
+            return DataValidationErrors.EmptyField;
+        }
 
         var gameNetworkData = ScriptableObject.CreateInstance<GameNetworkData>();
 
         gameNetworkData.netMode = netMode;
         gameNetworkData.portNum = portNum;  
+
+        if(!PortNumDataValidation(portNum))
+        {
+            return DataValidationErrors.PortNumError;
+        }
+
         gameNetworkData.iPAddress = iPAddress;
+
+        if (!IPAddressDataValidation(iPAddress))
+        {
+            return DataValidationErrors.IPAddressError;
+        }
+
         gameNetworkData.playerName = playerName;
 
         _gameNetworkDataList.Add(gameNetworkData);
+
+        return DataValidationErrors.None;
     }
 
+    private bool PortNumDataValidation(string portNum)
+    {
+        for(int i=0; i<portNum.Length; i++)
+        {
+            if (!(portNum[i] >=48 && portNum[i] <=57))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private bool IPAddressDataValidation(string iPAddress)
+    {
+        for (int i = 0; i < iPAddress.Length; i++)
+        {
+            if (iPAddress[i] == ' ')
+            {
+                return false;
+            }
+        }
+
+        string[] splitValues = iPAddress.Split('.');
+
+        if(splitValues.Length != 4 )
+        {
+            return false;
+        }
+
+        for(int i=0; i<splitValues.Length; i++)
+        {
+            if (splitValues[i].Length > 3 || splitValues[i].Length < 1)
+            {
+                return false;
+            }
+
+            string value = splitValues[i];
+
+            if(value.Length == 3)
+            {
+                if (!(value[0] >= 48 && value[0] <= 50))
+                {
+                    return false;
+                }
+                else if(!(value[1] >= 48 && value[1] <= 53))
+                {
+                    return false;
+                }
+                else if (!(value[2] >= 48 && value[2] <= 53))
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
 }
--- Assets/_Game/Scripts/UI/MatchmakingUiController.cs
+++ Assets/_Game/Scripts/UI/MatchmakingUiController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.Events;
 using TMPro;
+using System.Collections;
 
 public class MatchmakingUiController : MonoBehaviour
 {
@@ -14,6 +15,11 @@ public class MatchmakingUiController : MonoBehaviour
     [SerializeField] private TMP_InputField _hostIP;
     [SerializeField] private TMP_InputField _portNum;
 
+    [SerializeField] private GameObject _dataValidationErrors;
+    private TextMeshProUGUI _errorText;
+
+    private NetworkSessionManager.DataValidationErrors _validationError;
+
     private void Awake()
     {
         if (Instance != null && Instance != this)
@@ -26,23 +32,70 @@ public class MatchmakingUiController : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        _errorText = _dataValidationErrors.GetComponentInChildren<TextMeshProUGUI>();
+    }
+
     public void BackClick()
     {
         OnBackClick.Invoke();
     }
 
     public void JoinGame()
-    {
-        OnJoinGameClick.Invoke();
+    { 
+        _validationError = NetworkSessionManager.Instance.AddGameNetworkData(GameNetworkData.ENetMode.Client, _portNum.text, _hostIP.text, _playerName.text);
 
-        
-        NetworkSessionManager.Instance.AddGameNetworkData(GameNetworkData.ENetMode.Client, _portNum.text, _hostIP.text, _playerName.text);
+        if(_validationError != NetworkSessionManager.DataValidationErrors.None)
+        {
+            OnErrorBehavior(_validationError);
+        }
+        else
+        {
+            _dataValidationErrors.SetActive(false);
+            OnJoinGameClick.Invoke();
+        }
     }
 
     public void HostGame()
     {
-        OnHostGameClick.Invoke();
+        _validationError = NetworkSessionManager.Instance.AddGameNetworkData(GameNetworkData.ENetMode.Host, _portNum.text, _hostIP.text, _playerName.text);
 
-        NetworkSessionManager.Instance.AddGameNetworkData(GameNetworkData.ENetMode.Host, _portNum.text, _hostIP.text, _playerName.text);
+        if (_validationError != NetworkSessionManager.DataValidationErrors.None)
+        {
+            OnErrorBehavior(_validationError);
+        }
+        else
+        {
+            _dataValidationErrors.SetActive(false);
+            OnHostGameClick.Invoke();
+        }
+    }
+
+    private void OnErrorBehavior(NetworkSessionManager.DataValidationErrors _validationError)
+    {
+        if(_validationError == NetworkSessionManager.DataValidationErrors.EmptyField)
+        {
+            _errorText.text = "Please fill in the required field.";
+        }
+        else if(_validationError == NetworkSessionManager.DataValidationErrors.PortNumError)
+        {
+            _errorText.text = "Please provide a valid port number.";
+        }
+        else
+        {
+            _errorText.text = "Invalid IP address. Please enter a valid IP address.";
+        }
+
+        StartCoroutine(ErrorUI());
+    }
+
+    private IEnumerator ErrorUI()
+    {
+        _dataValidationErrors.SetActive(true);
+
+        yield return new WaitForSeconds(2);
+
+        _dataValidationErrors.SetActive(false);
     }
 }
