d16aaa634379099e4e927681fdc070b8791bace6
zanetazuranska <95565830+zanetazuranska@users.noreply.github.com>
2024-02-03 12:58:27 +0100

    Faces

--- Assets/_Game/Scripts/Networking/PlayerNetworkController.cs
+++ Assets/_Game/Scripts/Networking/PlayerNetworkController.cs
@@ -9,6 +9,31 @@ namespace ER
 
         private Vector3 _spawnPosition = new Vector3(-10.9f, 7.7f, -13.47f);
 
+        [SerializeField] private GameObject _faceHost;
+        [SerializeField] private GameObject _faceClient;
+
+        [ClientRpc]
+        private void SetClientFaceClientRpc()
+        {
+            if (IsHost) return;
+
+            _faceClient.SetActive(true);
+        }
+
+        [ClientRpc]
+        private void SetHostFaceClientRpc()
+        {
+            if(!IsHost) return;
+
+            _faceHost.SetActive(true);
+        }
+
+        [ServerRpc (RequireOwnership = false)]
+        private void InvokeOnHostServerRpc()
+        {
+            SetHostFaceClientRpc();
+        }
+
         public override void OnNetworkSpawn()
         {
             base.OnNetworkSpawn();
@@ -22,16 +47,28 @@ namespace ER
             if(IsHost)
             {
                 Debug.Log("Host");
+
+                _faceHost.SetActive(true);
+
                 EscapeRoomApp.Instance.OnHostSpawned.Invoke();
             }
 
             if (IsClient && !IsHost)
             {
                 Debug.Log("Client");
+
+                _faceClient.SetActive(true);
+
                 EscapeRoomApp.Instance.OnClientSpawned.Invoke();
+
             }
 
             GarageDoor.Instance.RegisterPlayer(this.gameObject);
+        }
+
+        private void OnDestroy()
+        {
+            EscapeRoomApp.Instance.OnClientSpawned.RemoveListener(SetClientFaceClientRpc);
         }
     }
 }
--- Assets/_Game/Scripts/Player/PlayerController.cs
+++ Assets/_Game/Scripts/Player/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
